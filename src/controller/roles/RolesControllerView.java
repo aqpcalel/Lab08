476fb7978ca4de4ea6172725cb606fc6c800d2bd
Araozu <tbyuyfcjjll@gmail.com>
2018-06-07 16:50:55 -0500

    Lanzamiento Inicial. Intenten crear branches para trabajar de forma segura.

---
+++ src/controller/roles/RolesControllerView.java
@@ -0,0 +1,18 @@
+package controller.roles;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@SuppressWarnings("serial")
+public class RolesControllerView extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
