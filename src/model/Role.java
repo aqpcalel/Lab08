8eb602ca1682fdd8217bbd8a1d3687690886c9c8
Araozu <tbyuyfcjjll@gmail.com>
2018-06-11 21:56:48 -0500

    Funcionalidad basica.

--- src/model/Role.java
+++ src/model/Role.java
@@ -1,4 +1,28 @@
 package model;
 
+import com.google.appengine.api.datastore.Key;
+
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+@PersistenceCapable
 public class Role {
+
+    @PrimaryKey
+    @Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+    private Key key;
+
+    @Persistent
+    private String name;
+
+    public Role(String name){
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Role name: " + name +"\n";
+    }
 }
